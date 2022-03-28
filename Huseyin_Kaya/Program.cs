806bff8b0baf42804bb8afa6b2e26e1ba0a82928
kkaya <kkaya@DESKTOP-I15BJPM>
2022-03-28 13:15:48 +0300

    Add project files.

---
+++ Huseyin_Kaya/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huseyin_Kaya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string m;
+            int i;
+            Console.WriteLine("İsim");
+            m = Console.ReadLine();
+
+            for (i = 1; i <= 20; i++)
+            {
+                Console.SetCursorPosition(30, i);
+                Console.Write(m);
+                Console.SetCursorPosition(55, i);
+                Console.Write(m);
+                Console.SetCursorPosition(42, 10);
+                Console.Write(m);
+                Console.SetCursorPosition(42, 11);
+                Console.Write(m);
+            }
+            for (i = 1; i <= 20; i++)
+            {
+                Console.SetCursorPosition(80, i);
+                Console.Write(m);
+                for (int j = 10; j < 21; j++)
+                {
+                    Console.SetCursorPosition(j + 80, j);
+                    Console.Write(m);
+                    Console.SetCursorPosition(110 - j, j - 9);
+                    Console.Write(m);
+                }
+
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
